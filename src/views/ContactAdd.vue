ecaf91fe443ee09a93032073a48d11a484f5b442
Ho Anh Truong <truongdc24v7x336@dttx.ctu.edu.vn>
2025-11-18 15:15:38 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="page">
+    <h4>Thêm mới Liên hệ</h4>
+
+    <ContactForm :contact="contact" @submit:contact="createContact" />
+
+    <p v-if="message" class="mt-2">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: {
+    ContactForm,
+  },
+
+  data() {
+    return {
+      contact: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false,
+      },
+      message: "",
+    };
+  },
+
+  methods: {
+    async createContact(data) {
+      try {
+        await ContactService.create(data);
+
+        alert("Thêm liên hệ thành công.");
+        this.$router.push({ name: "contactbook" });
+      } catch (error) {
+        console.log(error);
+        const errMsg =
+          error?.response?.data?.message ||
+          error?.message ||
+          "Đã có lỗi khi thêm liên hệ.";
+        this.message = errMsg;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.page {
+  max-width: 750px;
+  text-align: left;
+}
+</style>
